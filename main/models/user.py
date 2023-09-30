cc5c4466f338337359bfe1846358ccd82e590e88
denis <write.to.denis.today@gmail.com>
2023-09-30 16:56:17 +0200

    updated user model

--- main/models/user.py
+++ main/models/user.py
@@ -1,5 +1,19 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
-class User(AbstractUser):
-    pass
+# In accordance with the assignment, the generated code needs to be commented
+# FIXME: delete comments
+# TODO: add admin settings for this models
+# class User(AbstractUser):
+#     class Roles(models.TextChoices):
+#         DEVELOPER = "developer"
+#         MANAGER = "manager"
+#         ADMIN = "admin"
+#
+#     last_name = models.CharField(max_length=50, blank=False)
+#     first_name = models.CharField(max_length=50, blank=False)
+#     role = models.CharField(
+#         max_length=255, choices=Roles.choices, default=Roles.DEVELOPER
+#     )
+#     email = models.EmailField(unique=True)
