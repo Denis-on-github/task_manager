26a3f2246ab4af83e17547cd48ca469faed58767
denis <write.to.denis.today@gmail.com>
2023-09-30 17:32:33 +0200

    uncomment models

--- main/models/tag.py
+++ main/models/tag.py
@@ -1,8 +1,12 @@
 from django.db import models
 
+
 # In accordance with the assignment, the generated code needs to be commented
 # FIXME: delete comments
 # TODO: add admin settings for this models
 # About unique identifier field, it will be `id` field which Django generate automatically
-# class Tag(models.Model):
-#     title = models.CharField(max_length=255)
+class Tag(models.Model):
+    title = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.title
--- main/models/user.py
+++ main/models/user.py
@@ -5,15 +5,15 @@ from django.db import models
 # In accordance with the assignment, the generated code needs to be commented
 # FIXME: delete comments
 # TODO: add admin settings for this models
-# class User(AbstractUser):
-#     class Roles(models.TextChoices):
-#         DEVELOPER = "developer"
-#         MANAGER = "manager"
-#         ADMIN = "admin"
-#
-#     last_name = models.CharField(max_length=50, blank=False)
-#     first_name = models.CharField(max_length=50, blank=False)
-#     role = models.CharField(
-#         max_length=255, choices=Roles.choices, default=Roles.DEVELOPER
-#     )
-#     email = models.EmailField(unique=True)
+class User(AbstractUser):
+    class Roles(models.TextChoices):
+        DEVELOPER = "developer"
+        MANAGER = "manager"
+        ADMIN = "admin"
+
+    last_name = models.CharField(max_length=50, blank=False)
+    first_name = models.CharField(max_length=50, blank=False)
+    role = models.CharField(
+        max_length=255, choices=Roles.choices, default=Roles.DEVELOPER
+    )
+    email = models.EmailField(unique=True)
