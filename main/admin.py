5b8e5c6b80d5bb2826001931b8a399526f744a29
denis <write.to.denis.today@gmail.com>
2023-09-30 17:34:10 +0200

    formatted by black

--- main/admin.py
+++ main/admin.py
@@ -26,5 +26,5 @@ class TaskAdmin(admin.ModelAdmin):
 
 @admin.register(User, site=task_manager_admin_site)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('username', 'email', 'first_name', 'last_name', 'role')
-    search_fields = ('username', 'email', 'first_name', 'last_name')
+    list_display = ("username", "email", "first_name", "last_name", "role")
+    search_fields = ("username", "email", "first_name", "last_name")
