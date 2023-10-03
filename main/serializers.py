f49d0b94db83e51487b1f90edbfd9242b88a94b3
denis <write.to.denis.today@gmail.com>
2023-10-03 10:04:25 +0200

    added serializers

---
+++ main/serializers.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+
+from main.models import User, Tag, Task
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'first_name', 'last_name', 'email', 'date_of_birth', 'phone')
+
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = ('id', 'title')
+
+
+class TaskSerializer(serializers.ModelSerializer):
+    author = UserSerializer()
+    executor = UserSerializer()
+    tags = TagSerializer(many=True)
+
+    class Meta:
+        model = Task
+        fields = ('id', 'title', 'description', 'created_date', 'updated_date', 'due_date',
+                  'status', 'priority', 'author', 'executor', 'tags')
