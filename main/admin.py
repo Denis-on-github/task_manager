233fb7fb1e4d0607977e9b14753ae39e97df5758
denis <write.to.denis.today@gmail.com>
2023-10-04 09:59:00 +0200

    added role search field to the User in admin panel

--- main/admin.py
+++ main/admin.py
@@ -27,4 +27,4 @@ class TaskAdmin(admin.ModelAdmin):
 @admin.register(User, site=task_manager_admin_site)
 class UserAdmin(admin.ModelAdmin):
     list_display = ("username", "email", "first_name", "last_name", "role")
-    search_fields = ("username", "email", "first_name", "last_name")
+    search_fields = ("username", "email", "first_name", "last_name", "role")
