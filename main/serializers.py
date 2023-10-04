acb9e984101ad2ce1025e42090d6d6308ae7244d
denis <write.to.denis.today@gmail.com>
2023-10-04 11:12:46 +0200

    fixed fields in User model and user serializer

--- main/serializers.py
+++ main/serializers.py
@@ -8,12 +8,10 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = (
             "id",
-            "username",
             "first_name",
             "last_name",
             "email",
-            "date_of_birth",
-            "phone",
+            "role",
         )
 
 
