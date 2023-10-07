6bacd1a19b0d13b49dcd14e999883405ffbd0005
denis <write.to.denis.today@gmail.com>
2023-10-07 17:21:51 +0200

    added rest_access_policy in INSTALLED_APPS

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     "main",
     "rest_framework",
     "django_filters",
+    'rest_access_policy'
 ]
 
 MIDDLEWARE = [
@@ -63,7 +64,7 @@ REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
     "DEFAULT_PERMISSION_CLASSES": [
-        "rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly"
+        "rest_framework.permissions.IsAuthenticated"
     ],
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.SessionAuthentication",
