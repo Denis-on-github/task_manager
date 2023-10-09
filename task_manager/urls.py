2db2d83fd61f3f10130cf7e5d3c1c1f2ad6e5f7b
denis <write.to.denis.today@gmail.com>
2023-10-09 09:33:44 +0200

    added tasks and tags ViewSets to rhe routes

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -19,10 +19,12 @@ from django.urls import path, include
 from rest_framework import routers
 
 from main.admin import task_manager_admin_site
-from main.views import UserViewSet
+from main.views import UserViewSet, TaskViewSet, TagViewSet
 
 router = routers.SimpleRouter()
 router.register(r'users', UserViewSet, basename='users')
+router.register(r'tasks', TaskViewSet, basename='tasks')
+router.register(r'tags', TagViewSet, basename='tags')
 
 urlpatterns = [
     path("admin/", task_manager_admin_site.urls),
