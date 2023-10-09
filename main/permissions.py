2f36d203d772da1fd090c9f0056dde057de7e427
denis <write.to.denis.today@gmail.com>
2023-10-09 11:08:55 +0200

    updated permissions according to the task

--- main/permissions.py
+++ main/permissions.py
@@ -2,13 +2,7 @@ from rest_access_policy import AccessPolicy
 
 
 class IsStaffOrReadOnly(AccessPolicy):
-    def get_permissions(self, request, view, obj):
-        if request.method in ["GET", "HEAD", "OPTIONS"]:
-            return True
-        return request.user and request.user.is_staff
-
     def has_permission(self, request, view):
-        return self.get_permissions(request, view, None)
-
-    def has_object_permission(self, request, view, obj):
-        return self.get_permissions(request, view, obj)
+        if request.method == 'DELETE':
+            return request.user.is_staff
+        return True
