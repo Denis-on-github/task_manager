cfffb4ed356e17c8c501973c4a71ba8c1049af9d
denis <write.to.denis.today@gmail.com>
2023-10-09 09:38:12 +0200

    fixed tags route

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -19,12 +19,12 @@ from django.urls import path, include
 from rest_framework import routers
 
 from main.admin import task_manager_admin_site
-from main.views import UserViewSet, TaskViewSet
+from main.views import UserViewSet, TaskViewSet, TagViewSet
 
 router = routers.SimpleRouter()
 router.register(r'users', UserViewSet, basename='users')
 router.register(r'tasks', TaskViewSet, basename='tasks')
-router.register(r'tags', TaskViewSet, basename='tags')
+router.register(r'tags', TagViewSet, basename='tags')
 
 urlpatterns = [
     path("admin/", task_manager_admin_site.urls),
