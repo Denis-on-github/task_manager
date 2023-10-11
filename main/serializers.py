fca0f81be78334886983754495320e681142eb1c
denis <write.to.denis.today@gmail.com>
2023-10-11 10:58:42 +0200

    black formatted

--- main/serializers.py
+++ main/serializers.py
@@ -10,7 +10,7 @@ class UserSerializer(serializers.ModelSerializer):
             "id",
             "first_name",
             "last_name",
-            'username',
+            "username",
             "email",
             "role",
         )
