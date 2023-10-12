d5dcee2ada2cfead1b8db83d260770561806ed91
denis <write.to.denis.today@gmail.com>
2023-10-12 11:18:15 +0200

    added date_of_birth and phone to the User model

--- main/models/user.py
+++ main/models/user.py
@@ -14,7 +14,9 @@ class User(AbstractUser):
     role = models.CharField(
         max_length=255, choices=Roles.choices, default=Roles.DEVELOPER
     )
-    email = models.EmailField(unique=True)
+    email = models.EmailField(blank=False, unique=True)
+    date_of_birth = models.DateField(null=True, blank=True)
+    phone = models.CharField(max_length=20, null=True, blank=True, unique=True)
 
     def __str__(self):
         return self.username
