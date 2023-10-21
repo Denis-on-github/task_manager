4e9f338e04c0e3b2e59650ac1e5191a9a4307935
denis <write.to.denis.today@gmail.com>
2023-10-21 17:50:26 +0200

    deleted rollbar test route

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -53,5 +53,4 @@ urlpatterns = [
     ),
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-    path('api/rollbar/', index, name='rollbar')
 ]
