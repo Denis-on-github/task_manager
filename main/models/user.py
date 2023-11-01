1a41ed21c69b60626fa59572ca9a12e7a33a2aae
denis <write.to.denis.today@gmail.com>
2023-11-01 16:57:03 +0100

    updated user model

--- main/models/user.py
+++ main/models/user.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from main.services.storage_backends import public_storage
 
 
 class User(AbstractUser):
@@ -17,6 +18,7 @@ class User(AbstractUser):
     email = models.EmailField(blank=False, unique=True)
     date_of_birth = models.DateField(null=True, blank=True)
     phone = models.CharField(max_length=20, null=True, blank=True, unique=True)
+    avatar_picture = models.ImageField(null=True, storage=public_storage)
 
     def __str__(self):
         return self.username
