443284ae89a7af737ba47801fe22caf7ae7a4e6a
denis <write.to.denis.today@gmail.com>
2023-11-04 15:45:16 +0100

    added CurrentUserViewSet

--- main/views.py
+++ main/views.py
@@ -1,3 +1,5 @@
+from typing import cast
+
 import django_filters
 
 from rest_framework import viewsets
@@ -6,6 +8,7 @@ from rest_framework.permissions import IsAuthenticated
 from main.models import Tag, Task, User
 from main.permissions import IsStaffOrReadOnly
 from main.serializers import TagSerializer, TaskSerializer, UserSerializer
+from services.single_resource import SingleResourceMixin, SingleResourceUpdateMixin
 
 
 class UserFilter(django_filters.FilterSet):
@@ -20,6 +23,16 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.order_by("id")
     serializer_class = UserSerializer
     filterset_class = UserFilter
+
+
+class CurrentUserViewSet(
+    SingleResourceMixin, SingleResourceUpdateMixin, viewsets.ModelViewSet
+):
+    serializer_class = UserSerializer
+    queryset = User.objects.order_by("id")
+
+    def get_object(self) -> User:
+        return cast(User, self.request.user)
 
 
 class TagViewSet(viewsets.ModelViewSet):
